812e881c4340b3522e6a0a1421fc899c04e9ac14
Spinicle <70312979+Spinicle@users.noreply.github.com>
2026-04-17 12:06:30 -0400

    object_write: build header and compute object hash

--- object.c
+++ object.c
@@ -94,7 +94,29 @@ int object_exists(const ObjectID *id) {
 //
 // Returns 0 on success, -1 on error.
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out) {
-    // TODO: Implement
+
+    const char *type_str;
+    if (type == OBJ_BLOB) type_str = "blob";
+    else if (type == OBJ_TREE) type_str = "tree";
+    else if (type == OBJ_COMMIT) type_str = "commit";
+    else return -1;
+
+    char header[64];
+    int header_len = snprintf(header, sizeof(header), "%s %zu", type_str, len);
+    if (header_len < 0) return -1;
+
+    size_t total_len = header_len + 1 + len;
+
+    uint8_t *full = malloc(total_len);
+    if (!full) return -1;
+
+    memcpy(full, header, header_len);
+    full[header_len] = '\0';
+    memcpy(full + header_len + 1, data, len);
+
+    compute_hash(full, total_len, id_out);
+
+    
     (void)type; (void)data; (void)len; (void)id_out;
     return -1;
 }
