28a3114f5648e13a409ffbd74cbb850d010eea21
Spinicle <70312979+Spinicle@users.noreply.github.com>
2026-04-17 13:18:55 -0400

    feat(commit): update HEAD ref to point to newly created commit

--- commit.c
+++ commit.c
@@ -222,6 +222,7 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     }
     free(data);
 
-    (void)message; (void)commit_id_out;
-    return -1;
+    if (head_update(commit_id_out) != 0) return -1;
+
+    return 0;
 }
